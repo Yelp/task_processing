130ef1ea93f1486577cc793ac991bfc403a5a92b
Joseph Lynch <jlynch@yelp.com>
2017-05-12 17:56:38 -0700

    Fixup task_executor to be under 80 lines

--- task_processing/interfaces/task_executor.py
+++ task_processing/interfaces/task_executor.py
@@ -6,8 +6,12 @@ import six
 
 TaskConfig = namedtuple(
     'TaskConfig',
-    ['task_id', 'name', 'image', 'cmd', 'cpus', 'mem', 'disk', 'volumes', 'ports', 'cap_add',
-     'ulimit', 'docker_parameters'],
+    [
+        'task_id', 'name', 'image', 'cmd',
+        'cpus', 'mem', 'disk',
+        'volumes', 'ports', 'cap_add',
+        'ulimit', 'docker_parameters'
+    ],
 )
 
 # TODO: reimplement TaskConfig using attrs or precord
@@ -36,8 +40,10 @@ def make_task_config(image="ubuntu:xenial", cmd="/bin/true", cpus=0.1,
     if docker_parameters:
         docker_parameters = []
 
-    return TaskConfig(task_id, name, image, cmd, cpus, mem, disk, volumes, ports, cap_add,
-                      ulimit, docker_parameters)
+    return TaskConfig(
+        task_id, name, image, cmd, cpus, mem, disk, volumes, ports, cap_add,
+        ulimit, docker_parameters
+    )
 
 
 @six.add_metaclass(abc.ABCMeta)
