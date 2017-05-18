374d8e4998ab1e2575aa9d124618866469c34bc3
Maksym Melnychok <maksym@yelp.com>
2017-05-18 13:25:28 +0200

    fix invariants, random initial uuid

--- task_processing/interfaces/task_executor.py
+++ task_processing/interfaces/task_executor.py
@@ -1,22 +1,29 @@
 import abc
 import uuid
-from pyrsistent import PRecord, field, v
+from pyrsistent import PRecord, PVector, field, v, pvector
 
 import six
 
 class TaskConfig(PRecord):
-    uuid = field(type=uuid.UUID)
+    uuid = field(type=uuid.UUID, initial=uuid.uuid4)
     name = field(type=str, initial="default")
     image = field(type=str, initial="ubuntu:xenial")
     cmd = field(type=str, initial="/bin/true")
-    cpus = field(type=float, initial=0.1, invariant=lambda c: c > 0)
-    mem = field(type=float, initial=32, invariant=lambda m: m >= 32)
-    disk = field(type=float, initial=10, invariant=lambda d: d > 0)
-    volumes = field(type=pvector, initial=v(), factory=pvector)
-    ports = field(type=pvector, initial=v(), factory=pvector)
-    cap_add = field(type=pvector, initial=v(), factory=pvector)
-    ulimit = field(type=pvector, initialv=(), factory=pvector)
-    docker_parameters = field(type=pvector, initial=v(), factory=pvector)
+    cpus = field(type=float,
+                 initial=0.1,
+                 invariant=lambda c: (c > 0, 'cpus > 0'))
+    mem = field(type=float,
+                initial=32.0,
+                invariant=lambda m: (m >= 32, 'mem is >= 32'))
+    disk = field(type=float,
+                 initial=10.0,
+                 invariant=lambda d: (d > 0, 'disk > 0'))
+    volumes = field(type=PVector, initial=v(), factory=pvector)
+    ports = field(type=PVector, initial=v(), factory=pvector)
+    cap_add = field(type=PVector, initial=v(), factory=pvector)
+    ulimit = field(type=PVector, initial=v(), factory=pvector)
+    # TODO: containerization + containerization_args ?
+    docker_parameters = field(type=PVector, initial=v(), factory=pvector)
 
     def task_id():
         return "{}.{}".format(self.name, str(self.uuid))
