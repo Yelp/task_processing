90d9e8cb163509a661d85d62f78198dff7a46dc7
Robert Johnson <robj@yelp.com>
2017-06-15 07:44:28 -0700

    ensure stateful executor proxies events to parent

--- task_processing/plugins/stateful/stateful_executor.py
+++ task_processing/plugins/stateful/stateful_executor.py
@@ -10,11 +10,14 @@ class StatefulTaskExecutor(TaskExecutor):
     def __init__(self, downstream_executor, persister):
         self.downstream_executor = downstream_executor
         self.writer_queue = Queue()
+        self.queue_for_processed_events = Queue()
         worker = worker_for_event_queue(
-            event_queue=self.event_queue(),
-            persister=persister
+            event_queue=self.downstream_executor.get_event_queue(),
+            persister=persister,
+            on_process=lambda event:
+                put_to_outbound_queue(event, self.queue_for_processed_events)
         )
-        worker_thread = threading.Thread(target=worker)
+        worker_thread = threading.Thread(target=worker, daemon=True)
         worker_thread.start()
 
     def run(self, task_config):
@@ -27,16 +30,19 @@ class StatefulTaskExecutor(TaskExecutor):
     def status(self, task_id):
         pass
 
-    def event_queue(self):
-        return self.downstream_executor.event_queue()
+    def get_event_queue(self):
+        return self.queue_for_processed_events
 
-
-
-def worker_for_event_queue(event_queue, persister):
+def worker_for_event_queue(event_queue, persister, on_process):
     def subscribe_to_updates_for_task():
         while True:
             result = event_queue.get()
             persister.write(event=result)
+            on_process(result)
             event_queue.task_done()
 
     return subscribe_to_updates_for_task
+
+def put_to_outbound_queue(event, processed_queue):
+    print("processed event {}".format(event))
+    processed_queue.put(event)
