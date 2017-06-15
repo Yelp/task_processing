d1f06e02c26577363750545172524788649a9a5f
Robert Johnson <robj@yelp.com>
2017-06-15 06:37:46 -0700

    add file persistence plugin

---
+++ task_processing/plugins/persistence/file_persistence.py
@@ -0,0 +1,16 @@
+import json
+import os
+from task_processing.plugins.persistence.reader import Reader
+from task_processing.plugins.persistence.writer import Writer
+
+class FilePersistence(Reader, Writer):
+    def __init__(self, output_file):
+        self.output_file = output_file
+        pass
+
+    def read(self, task_id):
+        pass
+
+    def write(self, event):
+        with open(self.output_file, 'a+') as f:
+            f.write("{}\n".format(json.dumps(event.serialize())))
