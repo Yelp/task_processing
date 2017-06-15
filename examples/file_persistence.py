959b8954e6d9e163db728b6da350e35802140080
Robert Johnson <robj@yelp.com>
2017-06-15 06:43:06 -0700

    add example usage of file persistence

---
+++ examples/file_persistence.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import logging
+import os
+
+from task_processing.plugins.mesos.mesos_executor import MesosExecutor
+from task_processing.plugins.stateful.file_persistence import FilePersistence
+from task_processing.plugins.stateful.stateful_executor import StatefulTaskExecutor
+from task_processing.runners.sync import Sync
+
+logging.basicConfig()
+
+
+def main():
+    mesos_address = '0.0.0.0:32843'
+    mesos_executor = MesosExecutor(
+        credential_secret_file='./examples/cluster/secret',
+        mesos_address=mesos_address,
+        role='task-proc'
+    )
+    executor = StatefulTaskExecutor(
+        downstream_executor=mesos_executor,
+        persister=FilePersistence(
+            output_file='foo'
+        )
+    )
+    runner = Sync(executor=executor)
+    tasks = set()
+    TaskConfig = MesosExecutor.TASK_CONFIG_INTERFACE
+    for _ in range(1, 2):
+        task_config = TaskConfig(
+            image='ubuntu:14.04', cmd='/bin/sleep 2'
+        )
+        tasks.add(task_config.task_id)
+        runner.run(task_config)
+
+    print('Running {} tasks: {}'.format(len(tasks), tasks))
+
+
+if __name__ == '__main__':
+    main()
+    with open('foo') as f:
+        print(f.read())
+
+    exit(0)
