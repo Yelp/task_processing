de9e72cc9f4a4bb5d2f8f1c08acb2c84321ed67b
Robert Johnson <robj@yelp.com>
2017-06-15 06:35:58 -0700

    add reader and writer persistence interfaces

---
+++ task_processing/interfaces/persistence/reader.py
@@ -0,0 +1,9 @@
+import abc
+import six
+
+@six.add_metaclass(abc.ABCMeta)
+class Reader():
+
+    @abc.abstractmethod
+    def read(event):
+        pass
---
+++ task_processing/interfaces/persistence/writer.py
@@ -0,0 +1,9 @@
+import abc
+import six
+
+@six.add_metaclass(abc.ABCMeta)
+class Writer():
+
+    @abc.abstractmethod
+    def write():
+        pass
