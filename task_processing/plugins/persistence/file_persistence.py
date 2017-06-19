45ab30cc7bc02425a755eeb18bd396dea06cda8c
Robert Johnson <robj@yelp.com>
2017-06-19 09:31:00 -0700

    remove a rogue pass

--- task_processing/plugins/persistence/file_persistence.py
+++ task_processing/plugins/persistence/file_persistence.py
@@ -12,7 +12,6 @@ from task_processing.interfaces.persistence.writer import Writer
 class FilePersistence(Reader, Writer):
     def __init__(self, output_file):
         self.output_file = output_file
-        pass
 
     def read(self, task_id):
         acc = v()
