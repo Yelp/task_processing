85e4936616d9645ee0196df1154f8b113ba4fb34
Maksym Melnychok <maksym@yelp.com>
2017-08-18 12:47:34 +0200

    add sensible factories to mesos task config and test them

---
+++ tests/unit/plugins/mesos/mesos_task_config_test.py
@@ -0,0 +1,17 @@
+from task_processing.plugins.mesos.mesos_executor import MesosTaskConfig
+
+
+def test_mesos_task_config_factories():
+    m = MesosTaskConfig(cpus=1, mem=64, disk=15, gpus=6.0)
+
+    assert type(m.cpus) is float
+    assert m.cpus == 1.0
+
+    assert type(m.mem) is float
+    assert m.mem == 64.0
+
+    assert type(m.disk) is float
+    assert m.disk == 15.0
+
+    assert type(m.gpus) is int
+    assert m.gpus == 6
