a607987615ef34878c7fa9be3881c328a0a76d0f
Valeriy Khromov <vkhromov@yelp.com>
2018-04-17 22:38:41 -0700

    Implemented Promise task runner

--- examples/promise.py
+++ examples/promise.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python3
 import logging
-import os
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import wait
 
-from task_processing.executors.mesos_executor import MesosExecutor
-from task_processing.executors.task_executor import make_task_config
+from common import parse_args
+
 from task_processing.runners.promise import Promise
+from task_processing.task_processor import TaskProcessor
 
-
-logging.basicConfig()
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s'
+LEVEL = logging.DEBUG
+logging.basicConfig(format=FORMAT, level=LEVEL)
 
 
 def main():
-    credentials = {'principal': 'mesos', 'secret': 'very'}
-    mesos_address = os.environ['MESOS']
-    executor = MesosExecutor(
-        credentials=credentials,
-        mesos_address=mesos_address,
-        role='taskproc'
+    args = parse_args()
+    processor = TaskProcessor()
+    processor.load_plugin(provider_module='task_processing.plugins.mesos')
+    executor = processor.executor_from_config(
+        provider='mesos',
+        provider_config={
+            'secret': args.secret,
+            'mesos_address': args.master,
+            'pool': args.pool,
+            'role': args.role,
+        }
     )
-    task_config = make_task_config(image="busybox", cmd="/bin/true")
-    runner = Promise(executor)
-    future = runner.run(task_config)
-    print(future.get())
+
+    TaskConfig = executor.TASK_CONFIG_INTERFACE
+    task_config = TaskConfig(image="busybox", cmd='/bin/true')
+    # This only works on agents that have added mesos as a containerizer
+    # task_config = TaskConfig(containerizer='MESOS', cmd='/bin/true')
+
+    with ThreadPoolExecutor(max_workers=2) as futures_executor:
+        runner = Promise(executor, futures_executor)
+        future = runner.run(task_config)
+        wait([future])
+        result = future.result()
+        print(result)
+        print(result.raw)
+        runner.stop()
+
+    return 0 if result.success else 1
 
 
 if __name__ == "__main__":
--- task_processing/runners/promise.py
+++ task_processing/runners/promise.py
@@ -1,9 +1,24 @@
 from task_processing.interfaces.runner import Runner
+from task_processing.runners.sync import Sync
 
 
 class Promise(Runner):
-    def __init__(self, executor):
-        pass
+    def __init__(self, executor, futures_executor):
+        self.TASK_CONFIG_INTERFACE = executor.TASK_CONFIG_INTERFACE
+        self._futures_executor = futures_executor
+        self._runner = Sync(executor)
 
     def run(self, task_config):
-        pass
+        """Schedules execution of the supplied task
+
+        :param task_config: An object satisfying the TASK_CONFIG_INTERFACE
+
+        :return: A Future object representing the execution of the task.
+        """
+        return self._futures_executor.submit(self._runner.run, task_config)
+
+    def kill(self, task_id):
+        self._runner.kill(task_id)
+
+    def stop(self):
+        self._runner.stop()
