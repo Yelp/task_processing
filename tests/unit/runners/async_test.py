eb033c67fac6265cafb6bf974801ea5481a8d882
Qui Nguyen <1322636+qui@users.noreply.github.com>
2018-08-31 15:42:29 -0700

    Basic tests for runners

---
+++ tests/unit/runners/async_test.py
@@ -0,0 +1,39 @@
+import mock
+import pytest
+
+from task_processing.runners.async import Async
+
+
+@pytest.fixture
+def fake_executor():
+    return mock.Mock()
+
+
+@pytest.fixture
+def fake_runner(fake_executor):
+    runner = Async(
+        executor=fake_executor,
+        callbacks=[mock.Mock()],
+    )
+    yield runner
+    runner.stop()
+
+
+def test_run(fake_runner, fake_executor):
+    fake_runner.run(mock.Mock())
+    assert fake_executor.run.call_count == 1
+
+
+def test_reconcile(fake_runner, fake_executor):
+    fake_runner.reconcile(mock.Mock())
+    assert fake_executor.reconcile.call_count == 1
+
+
+def test_kill(fake_runner, fake_executor):
+    fake_runner.kill('some_id')
+    assert fake_executor.kill.call_count == 1
+
+
+def test_stop(fake_runner, fake_executor):
+    fake_runner.stop()
+    assert fake_executor.stop.call_count == 1
---
+++ tests/unit/runners/subscription_test.py
@@ -0,0 +1,39 @@
+import mock
+import pytest
+
+from task_processing.runners.subscription import Subscription
+
+
+@pytest.fixture
+def fake_executor():
+    return mock.Mock()
+
+
+@pytest.fixture
+def fake_runner(fake_executor):
+    runner = Subscription(
+        executor=fake_executor,
+        queue=mock.Mock(),
+    )
+    yield runner
+    runner.stop()
+
+
+def test_run(fake_runner, fake_executor):
+    fake_runner.run(mock.Mock())
+    assert fake_executor.run.call_count == 1
+
+
+def test_reconcile(fake_runner, fake_executor):
+    fake_runner.reconcile(mock.Mock())
+    assert fake_executor.reconcile.call_count == 1
+
+
+def test_kill(fake_runner, fake_executor):
+    fake_runner.kill('some_id')
+    assert fake_executor.kill.call_count == 1
+
+
+def test_stop(fake_runner, fake_executor):
+    fake_runner.stop()
+    assert fake_executor.stop.call_count == 1
