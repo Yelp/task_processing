63d3def1f7594a804a75e8d49fd42aca0bf65b0a
Luis Pérez <luisp@yelp.com>
2021-06-24 17:18:58 -0400

    Add a way to track task metadata in k8s plugin (#154)
    * Add a way to track task metadata in k8s plugin
    
    We'll use this in the same way as we handled things in the mesos plugin:
    run/kill will directly mutate the task_metadata member I'm adding in this PR
    and we'll eventually have the thread(s?) that handle watching Pods also
    add to this member in addition to the event queue that we'll share with
    Tron
    
    Unlike the mesos plugin, however, we now keep a PVector of task states.
    If we keep this the same as in the Mesos plugin (i.e., as a PMap) then
    we lose information if a task is bouncing between states.

---
+++ tests/unit/plugins/kubernetes/kubernetes_pod_executor_test.py
@@ -0,0 +1,41 @@
+import os
+from unittest import mock
+
+import pytest
+from pyrsistent import pmap
+from pyrsistent import v
+
+from task_processing.plugins.kubernetes.kubernetes_pod_executor import KubernetesPodExecutor
+from task_processing.plugins.kubernetes.kubernetes_pod_executor import KubernetesTaskMetadata
+from task_processing.plugins.kubernetes.kubernetes_pod_executor import KubernetesTaskState
+from task_processing.plugins.kubernetes.task_config import KubernetesTaskConfig
+
+
+@pytest.fixture
+def k8s_executor():
+    with mock.patch(
+        "task_processing.plugins.kubernetes.kube_client.kube_config.load_kube_config",
+        autospec=True
+    ), mock.patch(
+        "task_processing.plugins.kubernetes.kube_client.kube_client",
+        autospec=True
+    ), mock.patch.dict(os.environ, {"KUBECONFIG": "/this/doesnt/exist.conf"}):
+        executor = KubernetesPodExecutor()
+        yield executor
+        executor.stop()
+
+
+def test_run_updates_task_metadata(k8s_executor):
+    task_config = KubernetesTaskConfig(name="name", uuid="uuid")
+    k8s_executor.run(task_config=task_config)
+
+    assert k8s_executor.task_metadata == pmap(
+        {
+            task_config.pod_name: KubernetesTaskMetadata(
+                task_state_history=v((KubernetesTaskState.TASK_PENDING, mock.ANY)),
+                task_config=task_config,
+                node_name='',
+                task_state=KubernetesTaskState.TASK_PENDING,
+            ),
+        },
+    )
