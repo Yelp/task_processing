ffd1847c6ae2f313f1335f16a8787851fda7c901
Luis Perez <luisp@yelp.com>
2021-07-12 14:12:18 -0700

    Handle certificate expiry in k8s plugin
    We regualrly rotate certificates in our clusters and the k8s clientlib
    won't notice when these files change (nor does it have a way to add a
    hook on a 401), so manually catch any 401 API exceptions and reload the
    kubeconfig file.

--- task_processing/plugins/kubernetes/kube_client.py
+++ task_processing/plugins/kubernetes/kube_client.py
@@ -14,12 +14,31 @@ class KubeClient:
                 "or pass a value for `kube_config_path`!"
             )
 
+        self.kube_config_path = kube_config_path
+        self.kubecontext = os.environ.get("KUBECONTEXT")
+
         kube_config.load_kube_config(
-            config_file=kube_config_path,
-            context=os.environ.get("KUBECONTEXT")
+            config_file=self.kube_config_path,
+            context=self.kubecontext
         )
 
         # any Kubernetes APIs that we use should be added as members here (much like as we
         # do in the KubeClient class in PaaSTA) to ensure that they're only used after we've
-        # loaded the relevant kubeconfig
+        # loaded the relevant kubeconfig. Additionally, they should also be recreated in
+        # reload_kubeconfig() so that changes in certs are picked up.
+        self.core = kube_client.CoreV1Api()
+
+    def reload_kubeconfig(self) -> None:
+        """
+        Helper meant to be called after a 401 from apiserver.
+
+        Since we rotate certs regularly internally and the kubernetes clientlib doesn't
+        automatically watch for changes to the certs specified by the kubeconfig file, we
+        need to manually reload said file as well as recreate any API classes (as otherwise
+        they don't pick up the new configuration)
+        """
+        kube_config.load_kube_config(
+            config_file=self.kube_config_path,
+            context=self.kubecontext
+        )
         self.core = kube_client.CoreV1Api()
