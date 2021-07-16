ecbea2c6d7492aa2054e81a8ecf456f229631428
Luis Perez <luisp@yelp.com>
2021-07-16 09:14:07 -0700

    Add resource limits, caps, env vars, and volumes to k8s task_config
    We'll need these for:
    * resource limits: to isolate resource usage per-task as we did in Mesos
    * capabilties: to match the security status-quo in PaaSTA/Tron-on-Mesos
    * volumes: to allow people to mount files on disk :p
    * environment variables: we'll need these for both PaaSTA secrets as
      well as for normal usage (some tasks may configure
      themselves/libraries with environment variables)

--- tests/unit/plugins/kubernetes/kubernetes_task_config_test.py
+++ tests/unit/plugins/kubernetes/kubernetes_task_config_test.py
@@ -49,3 +49,115 @@ def test_kubernetes_task_config_enforces_command_requirmenets():
     )
     with pytest.raises(InvariantException):
         task_config.set(command="")
+
+
+@pytest.mark.parametrize(
+    "capabilties", (
+        ("NOT_A_CAP",),
+        ("MKNOD", "NOT_A_CAP",),
+    )
+)
+def test_cap_add_capabilities_rejects_invalid_capabilites(capabilties):
+    with pytest.raises(InvariantException):
+        KubernetesTaskConfig(
+            name="fake_task_name",
+            uuid="fake_id",
+            image="fake_docker_image",
+            command="fake_command",
+            cap_add=capabilties,
+        )
+
+
+@pytest.mark.parametrize(
+    "capabilties", (
+        ("CHOWN",),
+        ("MKNOD", "CHOWN",),
+    )
+)
+def test_cap_add_capabilities_valid_capabilites(capabilties):
+    task_config = KubernetesTaskConfig(
+        name="fake_task_name",
+        uuid="fake_id",
+        image="fake_docker_image",
+        command="fake_command",
+        cap_add=capabilties,
+    )
+    assert tuple(task_config.cap_add) == capabilties
+
+
+@pytest.mark.parametrize(
+    "capabilties", (
+        ("NOT_A_CAP",),
+        ("MKNOD", "NOT_A_CAP",),
+    )
+)
+def test_cap_drop_capabilities_rejects_invalid_capabilites(capabilties):
+    with pytest.raises(InvariantException):
+        KubernetesTaskConfig(
+            name="fake_task_name",
+            uuid="fake_id",
+            image="fake_docker_image",
+            command="fake_command",
+            cap_drop=capabilties,
+        )
+
+
+@pytest.mark.parametrize(
+    "capabilties", (
+        ("CHOWN",),
+        ("MKNOD", "CHOWN",),
+    )
+)
+def test_cap_drop_capabilities_valid_capabilites(capabilties):
+    task_config = KubernetesTaskConfig(
+        name="fake_task_name",
+        uuid="fake_id",
+        image="fake_docker_image",
+        command="fake_command",
+        cap_drop=capabilties,
+    )
+    assert tuple(task_config.cap_drop) == capabilties
+
+
+@pytest.mark.parametrize(
+    "volumes", (
+        [{"host_path": "/a"}],
+        [{"host_path": "/a", "containerPath": "/b"}],
+        [{"host_path": "/a", "containerPath": "/b", "mode": "RO"}],
+        [{"host_path": "/a", "container_path": "/b", "mode": "LOL"}],
+        [
+            {"host_path": "/c", "container_path": "/d", "mode": "RO"},
+            {"host_path": "/e", "containerPath": "/f", "mode": "LOL"}
+        ],
+    )
+)
+def test_volume_rejects_invalid_specification(volumes):
+    with pytest.raises(InvariantException):
+        KubernetesTaskConfig(
+            name="fake_task_name",
+            uuid="fake_id",
+            image="fake_docker_image",
+            command="fake_command",
+            volumes=volumes
+        )
+
+
+@pytest.mark.parametrize(
+    "volumes", (
+        ({"host_path": "/a", "container_path": "/b", "mode": "RO"},),
+        (
+            {"host_path": "/a", "container_path": "/b", "mode": "RO"},
+            {"host_path": "/c", "container_path": "/d", "mode": "RW"}
+        ),
+    )
+)
+def test_volume_valid_specification(volumes):
+    task_config = KubernetesTaskConfig(
+        name="fake_task_name",
+        uuid="fake_id",
+        image="fake_docker_image",
+        command="fake_command",
+        volumes=volumes
+    )
+
+    assert tuple(task_config.volumes) == volumes
